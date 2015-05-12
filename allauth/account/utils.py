5132b0c5509ed8b4ba00d36914a28f171b4ef42d
Sandeep Nangia <sandeep.nangia@gmail.com>
2015-05-13 00:32:29 +0530

    added debug print statement

--- allauth/account/utils.py
+++ allauth/account/utils.py
@@ -128,6 +128,7 @@ def perform_login(request, user, email_verification,
                     "Your confirmation code is %s" % confirmationCode)
         from rest_framework.status import is_success
         if not is_success(errcode):
+            print "username=%s", username
             print errmsg
             print "Error code = %d" % errcode
             return HttpResponseRedirect(
